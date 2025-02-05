0b506e47c2f3830ad3462613dc29ef1b3101f672
Jaynan Carrasquillo <jj.jaynan06@gmail.com>
2025-02-05 10:12:27 -0500

    python2

--- hangman.py
+++ hangman.py
@@ -95,33 +95,64 @@ def previousgues():
         pgues = prevgues[i] + " ,"
     return pgues 
 
+# "".join(hiddenword)
+
 while game_over == False:
     print(f"There are {len(word)} letters in the word. Guess a letter: ")
     print(HANGMANPICS[tries])
     print("Word:", hiddenword)
     prevgues.sort()
-    print("Previous guesses:", prevgues)
+    print(" ".join(hiddenword))#temporary#temporary#temporary#temporary#temporary#temporary#temporary#temporary
+    print("Previous guesses:", ", ".join(prevgues))
     print(word)#fdgilukdfhgyidfsuhisdfhgoisdfgkjlsdfhiousdfigudfhoiudsfhgdfhiguhsdflighsdfoiguhldifuhgosidfhgoisdfuhgoisdfghio
     guess = input("Letter to guess?: ")
-    prevgues.append(guess)
+    prevgues = list(prevgues)
+    
 
     if guess in word:
         for letter in word:
             
-            print(count)
+            print(count)#temporary#temporary#temporary#temporary#temporary#temporary#temporary
             #if count >= len(hiddenword):
                 #count = -1
                 #break
             if letter == guess:
-                count += 1
-                hiddenword[count] = guess
-
+                # count += 1
+                hiddenword[word.find(guess)] = guess
+                word1 = word[:word.find(guess)] + word[word.find(guess) + 1:]
+                if word1.find(guess) != -1:
+                    hiddenword[word1.find(guess)] = guess
+                    word2 = word[:word.find(guess)] + word[word.find(guess) + 1:]
+                    if word2.find(guess) != -1:
+                        hiddenword[word2.find(guess)] = guess
+                        word3 = word[:word.find(guess)] + word[word.find(guess) + 1:]
+                        if word3.find(guess) != -1:
+                            hiddenword[word3.find(guess)] = guess
 
     elif guess not in word:
         tries += 1
+        prevgues.append(guess)
 
     clear_screen()
 
+    if "".join(hiddenword) == word:
+        print("YOU WIN, HANGMAN LIVES!")
+        playagain = input("Play again? (y/n): ")
+        if playagain == "n":
+            game_over = True
+        elif playagain == "y":
+            game_over = False
+            tries = 0
+            count = -1
+            prevgues = []
+            word = random.choice(words)
+            prevgues = [ ]
+            hiddenword = "_" * len(word)
+            hiddenword = list(hiddenword)
+        else:
+            playagain = input("Play again? (y/n): ")
+    
+        
 
     if tries == 6:
         print("GAME OVER, HANGMAN DIED.")
