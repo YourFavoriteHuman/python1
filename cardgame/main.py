70f2700b2412230a382d4495070360cabc2a15ef
Jaynan Carrasquillo <jj.jaynan06@gmail.com>
2025-03-26 09:10:22 -0400

    3/26/25

--- cardgame/main.py
+++ cardgame/main.py
@@ -32,7 +32,7 @@ while gamerunning:
     
     #i played, com played, who has higher card?, remove a card and give both to the winner, if they both have to same card, its war. (bothput down 2 cards and compare who has the highest carf)
     
-    p1Table.pile_of_cards = [p1Hand.pile_of_cards.pop(random.randint(0, len(p1Hand.pile_of_cards) - 1)) for i in range(3)]
+    p1Table.pile_of_cards = [p1Hand.pile_of_cards.pop(random.randint(0, (len(p1Hand.pile_of_cards) - 1))) for i in range(3)]
     
     p2Table.pile_of_cards = [p1Hand.pile_of_cards.pop(random.randint(0, len(p1Hand.pile_of_cards) - 1)) for i in range(3)]
     print("I played: \n", " ".join(p1Table.pile_of_cards))
@@ -41,11 +41,11 @@ while gamerunning:
     
     if p1Table.pile_of_cards[-1] > p2Table.pile_of_cards[-1]:
         p1Table.pile_of_cards.extend([p2Table.pile_of_cards.pop(0), p2Table.pile_of_cards.pop(1), p2Table.pile_of_cards.pop(-1)])
-        print(p1Table.pile_of_cards)
+        print("\n", p1Table.pile_of_cards, "\n", p1Hand.pile_of_cards)
         print("Player win")
     elif p2Table.pile_of_cards[-1] > p1Table.pile_of_cards[-1]:
         p2Table.pile_of_cards.extend([p1Table.pile_of_cards.pop(0), p1Table.pile_of_cards.pop(1), p1Table.pile_of_cards.pop(-1)])
-        print(p2Table.pile_of_cards)
+        print("\n",p2Table.pile_of_cards, "\n", p1Hand.pile_of_cards)
         print("computer win")
     elif p1Table.pile_of_cards[-1] == p2Table.pile_of_cards[-1]:
         print("war time!")
