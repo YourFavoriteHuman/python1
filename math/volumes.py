b2d4cdf0bb57620b8869d5c51a0bfb838fccc59b
Jaynan Carrasquilo <jj.jaynan06@gmail.com>
2025-04-03 14:50:50 -0400

    4/3/2025-2

---
+++ math/volumes.py
@@ -0,0 +1,25 @@
+from math import *
+
+while True:
+    shape = input("which shape do u want to find volume for?(cube, prism, cyl, pyr, cone, exit): ")
+
+    if shape == "cube":
+        base = int(input("area of the base: "))
+    elif shape == "prism":
+        pass
+    elif shape == "cyl":
+        pass
+    elif shape == "pyr":
+        pass
+    elif shape == "cone":
+        pass
+    elif shape == "exit":
+        break
+    else:
+        print("invalid shape")
+    
+    
+    
+    
+    
+    
