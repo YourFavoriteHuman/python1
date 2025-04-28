a6ff67ac85807cc5644f7f9c3f81fdb4458e5ea5
Jaynan Carrasquillo <jj.jaynan06@gmail.com>
2025-04-28 10:23:13 -0400

    4/28/2025

--- graphics/contact saver/contactsaver.py
+++ graphics/contact saver/contactsaver.py
@@ -2,10 +2,12 @@ import tkinter as tk
 import csv
 
 root = tk.Tk()
-root.geometry("500x300")
-root.title("My App")
+root.geometry('800x500')
 
-# This is a 2D List
+root.attributes('-topmost', True)
+
+valid_users = [ ]
+'''
 students = [["3F","Jaynan Carrasquillo",9],	 		
             ["3F","Ethan Celano",10],	 		
             ["3F","Matheus Comandolli",11],	 		
@@ -25,23 +27,21 @@ students = [["3F","Jaynan Carrasquillo",9],
             ["7F","Parker Rutledge",10],	 		
             ["7F","Paul Vassiliadis",9]]
 
-# Create and Pack a text widget           
+# Create and grid a text widget           
 textArea = tk.Text(root, width=32, height=10)
-textArea.pack()
+textArea.grid(padx=5)
 
-# Loop through data and add to text widget
 count = 1
 for each_student in students:
     textArea.insert(tk.END, f"{count:2}: {each_student[1]}\n")
     count += 1
-        
+'''
     
 def save_data():
     filename = "data.csv"
     with open(filename, 'w', newline='') as file:
         writer = csv.writer(file)
-        writer.writerows(students)
-        
+        writer.writerows(username_str.get())        
 def load_data():
     with open('data.csv', 'r') as file:
         csv_reader = csv.reader(file)
@@ -53,4 +53,40 @@ def load_data():
 students2 = [ ]
 # load_data()
 
+def create_pushed():
+    valid_users.append( [username_str.get(), email_str.get() ] )
+    print(valid_users)
+    username.delete(0, tk.END)
+    email.delete(0, tk.END)
+    email.delete(0, tk.END)
+
+# Create Widgets
+username_str = tk.StringVar()
+username_label = tk.Label(root, text="Username:")
+username = tk.Entry(root, textvariable=username_str, width=20)
+username = tk.Entry(root, textvariable=username_str, width=20)
+
+email_str = tk.StringVar()
+email_label = tk.Label(root, text="Email:")
+email = tk.Entry(root, textvariable=email_str)
+
+phone_str = tk.StringVar()
+phone_label = tk.Label(root, text="Phone number:")
+phone = tk.Entry(root, textvariable=phone_str)
+
+login_button = tk.Button(root, text="Add Contact", command=create_pushed)
+
+textArea = tk.Text(root, width=70, height=20)
+# Layout Widgets
+
+textArea.grid(column=2, row=1, columnspan=2, rowspan=5)
+username_label.grid(pady=5, row=0, column=0)
+username.grid(pady=5, row=1, column=0)
+email_label.grid(pady=5, row=2, column=0)
+email.grid(pady=5, row=3, column=0)
+phone_label.grid(pady=5, row=4, column=0)
+phone.grid(pady=5, row=5, column=0)
+login_button.grid(pady=5, row=6, column=0)
+
+# Run main loop
 root.mainloop()
