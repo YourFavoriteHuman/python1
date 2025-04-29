7d78dfeda832e6c90215a4e3f52cb7a40a920791
Jaynan Carrasquillo <jj.jaynan06@gmail.com>
2025-04-29 10:23:25 -0400

    4/29/2025

--- graphics/contact saver/contactsaver.py
+++ graphics/contact saver/contactsaver.py
@@ -2,30 +2,13 @@ import tkinter as tk
 import csv
 
 root = tk.Tk()
-root.geometry('800x500')
-
+root.geometry('900x300')
+root.title("Contact Saver")
 root.attributes('-topmost', True)
+
 
 valid_users = [ ]
 '''
-students = [["3F","Jaynan Carrasquillo",9],	 		
-            ["3F","Ethan Celano",10],	 		
-            ["3F","Matheus Comandolli",11],	 		
-            ["3F","Camilo Contreras",11],	 		
-            ["3F","Nicolas Garcia-Macchiavello",12],	 		
-            ["3F","Amelia Gordon",11],	 		
-            ["3F","Hayden Green",11],	 		
-            ["3F","Philip Hanna",11],	 		
-            ["3F","Jace Nguyen",11],	 		
-            ["3F","Thien-Cat Nguyen",11],	 		
-            ["3F","Enzo Paparella",9],	 		
-            ["3F","Lars Scholz",12],	 		
-            ["3F","Logan Wagner",11],
-            ["7F","Claire Camchong",11],	 		
-            ["7F","Justin Jansen",12],	 		
-            ["7F","Olivia Nguyen",10],	 		
-            ["7F","Parker Rutledge",10],	 		
-            ["7F","Paul Vassiliadis",9]]
 
 # Create and grid a text widget           
 textArea = tk.Text(root, width=32, height=10)
@@ -46,47 +29,76 @@ def load_data():
     with open('data.csv', 'r') as file:
         csv_reader = csv.reader(file)
         for row in csv_reader:
-            students2.append(row)
-    print(students2)
+            valid_users.append(row)
+    print(valid_users)
     
 # save_data()
-students2 = [ ]
 # load_data()
 
 def create_pushed():
-    valid_users.append( [username_str.get(), email_str.get() ] )
+    valid_users.append( [username_str.get(), email_str.get(), phone_str.get()] )
+    # textArea.insert(tk.END, f"{username_str.get(), email_str.get(), phone_str.get()}\n")
+    textArea.insert(tk.END, f"{valid_users}\n")
+    valid_users.append( [username_str.get(), email_str.get(), phone_str.get()] )
     print(valid_users)
     username.delete(0, tk.END)
     email.delete(0, tk.END)
-    email.delete(0, tk.END)
+    phone.delete(0, tk.END)
+    print(textArea.get('1.0', tk.END))
 
 # Create Widgets
 username_str = tk.StringVar()
-username_label = tk.Label(root, text="Username:")
-username = tk.Entry(root, textvariable=username_str, width=20)
-username = tk.Entry(root, textvariable=username_str, width=20)
+username_label = tk.Label(root, text="Name:")
+username = tk.Entry(root, textvariable=username_str, width=40)
 
 email_str = tk.StringVar()
 email_label = tk.Label(root, text="Email:")
-email = tk.Entry(root, textvariable=email_str)
+email = tk.Entry(root, textvariable=email_str, width=40)
 
 phone_str = tk.StringVar()
 phone_label = tk.Label(root, text="Phone number:")
-phone = tk.Entry(root, textvariable=phone_str)
+phone = tk.Entry(root, textvariable=phone_str, width=40)
 
 login_button = tk.Button(root, text="Add Contact", command=create_pushed)
 
-textArea = tk.Text(root, width=70, height=20)
+textArea = tk.Text(root, width=70, height=12)
+textArea_label = tk.Label(root, text="All Contacts:")
+# textArea.insert(tk.END, f"{username_str.get()}\t{email_str.get()}\t{phone_str.get()}\n")
+
+
+#textArea.insert(tk.END, "Name\tEmail\tPhone\n")
+#textArea.insert(tk.END, "---------------------\n")
+
+#textArea.tag_configure("center", justify='center')
+#textArea.tag_add("center", "1.0", "end") # Center the text
+textArea.config(state=tk.DISABLED) # Make the text area read-only
+
+
+
+# Create a scrollbar
+
 # Layout Widgets
 
-textArea.grid(column=2, row=1, columnspan=2, rowspan=5)
-username_label.grid(pady=5, row=0, column=0)
+textArea.grid(column=2, row=1, columnspan=2, rowspan=5, padx=5) 
+textArea_label.grid(column=2, row=0, columnspan=2)
+username_label.grid(pady=5, row=0, column=0, sticky="w")
 username.grid(pady=5, row=1, column=0)
-email_label.grid(pady=5, row=2, column=0)
+email_label.grid(pady=5, row=2, column=0, sticky="w")
 email.grid(pady=5, row=3, column=0)
-phone_label.grid(pady=5, row=4, column=0)
+phone_label.grid(pady=5, row=4, column=0, sticky="w")
 phone.grid(pady=5, row=5, column=0)
 login_button.grid(pady=5, row=6, column=0)
 
 # Run main loop
 root.mainloop()
+
+'''
+textArea.grid(column=2, row=1, columnspan=2, rowspan=5)
+username_label.grid(pady=5, row=0, column=0, sticky="w")
+username.grid(pady=5, row=1, column=0)
+email_label.grid(pady=5, row=2, column=0, sticky="w")
+email.grid(pady=5, row=3, column=0)
+phone_label.grid(pady=5, row=4, column=0, sticky="w")
+phone.grid(pady=5, row=5, column=0)
+login_button.grid(pady=5, row=6, column=0)
+'''
